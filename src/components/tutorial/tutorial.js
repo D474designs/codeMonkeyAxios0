8555de5f08f461e324c7d95551bb9e84e7e6e4cf
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-09-15 14:33:39 -0500

    add tutorial server request prototype to be refactored into axios

--- src/components/tutorial/tutorial.js
+++ src/components/tutorial/tutorial.js
@@ -2,14 +2,24 @@ import YouTube from 'react-youtube';
 import React from 'react';
 
 class Tutorial extends React.Component {
-  render() {
-    const opts = {
-      height: '390',
-      width: '640',
-      playerVars: { // https://developers.google.com/youtube/player_parameters
-        autoplay: 2
-      }
-    };
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //       name: '',
+  //       description: '',
+  //       additional_desc: ''
+  //   }
+
+    // this.handleSubmit = this.handleSubmit.bind(this)
+
+    render() {
+      const opts = {
+        height: '390',
+        width: '640',
+        playerVars: { // https://developers.google.com/youtube/player_parameters
+          autoplay: 2
+        }
+      };
 
     return (
       <YouTube
@@ -18,12 +28,34 @@ class Tutorial extends React.Component {
         onReady={this._onReady}
       />
     );
+
+    // logChange(e) {
+    //     this.setState({[e.target.name]: e.target.value});
+    // }
+
   }
 
-  _onReady(event) {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
+  //  // refactor into axios
+  // getTutorial(){
+  //   $.ajax({
+  //     url:'/Tutorial',
+  //     type:"GET",
+  //     success: (data) => {
+  //       this.setState({
+  //         list: data
+  //       });
+  //     },
+  //     error: (xhr, err) => {
+  //       console.log('Can\'t get tutorial information', err);
+  //     }
+  //   })
+  // }
+
+    _onReady(event) {
+      // access to player in all event handlers via event.target
+      event.target.pauseVideo();
+    }
   }
-}
+// }
 
 export default Tutorial;
